09f8fe0826f6a12f3b62a25bd58f41ad8bda74f0
DataGobes <gcc.jansen@gmail.com>
2017-09-08 23:26:40 +0200

    ECDF added

--- functionplotter.py
+++ functionplotter.py
@@ -2,21 +2,46 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def plotter(data):
-    plt.plot(data)
+def plotter(data, start, stop):
+    x = np.arange(start=start, stop=stop, step=1)
+    plt.plot(x, data)
     plt.xlabel('x')
     plt.ylabel('y')
     plt.show()
 
 
-def linear(a, b, max):
-    data = [a*x + b for x in np.arange(max, step=1)]
-    return data
+def linear(a, b, start, stop):
+    """
+
+    :param a: linear coefficient
+    :param b: constant
+    :param start: minimum x value
+    :param stop: maximum x value
+    """
+    data = [a*x + b for x in np.arange(start=start, stop=stop, step=1)]
+    start = start
+    stop = stop
+    plotter(data, start, stop)
 
 
-def power(a, b, max):
-    data = [x ** a + b for x in np.arange(max, step=1)]
-    return data
+def power(a, b, start, stop):
+    """
+
+    :param a: function order
+    :param b: constant
+    :param start: minimum x value
+    :param stop: maximum x value
+    """
+    data = [x ** a + b for x in np.arange(start=start, stop=stop, step=1)]
+    start = start
+    stop = stop
+    plotter(data, start, stop)
+
+
+
+
+
+
 
 
 
